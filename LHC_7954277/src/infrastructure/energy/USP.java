01a266f84d1e201d71782237318cf512438b850e
digitalshoes <51914757+Kragu@users.noreply.github.com>
2020-02-07 15:13:28 +0100

    usp update

--- LHC_7954277/src/infrastructure/energy/USP.java
+++ LHC_7954277/src/infrastructure/energy/USP.java
@@ -2,9 +2,12 @@ package infrastructure.energy;
 
 import infrastructure.ThreePinPlug;
 
+import java.util.ArrayList;
+
 public class USP {
     private boolean isStandby;
     private boolean isActivated;
+    private ArrayList<Battery> batteryList;
     public void determineChargeState(){
 
 
@@ -16,5 +19,15 @@ public class USP {
     public int takeOut(){
 
         return 1;
+    }
+
+    public ArrayList<Battery> getBatteryList() {
+        return batteryList;
+    }
+
+    public USP(){
+        for(int i=0;i<25;i++){
+            batteryList.add(new Battery());
+        }
     }
 }
