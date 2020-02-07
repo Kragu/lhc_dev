42b03657770b4b2bdb2fc2cba923707ac37af006
StefanBrand42 <56312367+StefanBrand42@users.noreply.github.com>
2020-02-07 15:17:16 +0100

    test

---
+++ LHC_7954277/test/ApplicationTest.java
@@ -0,0 +1,55 @@
+import infrastructure.Building;
+import infrastructure.energy.Ring;
+import infrastructure.energy.USP;
+import lhc.LargeHadronCollider;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+int a;
+    @BeforeEach
+    void setUp() {
+        a = 5;
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+
+    @Order(1)
+    @Test
+    void test1(){
+        LargeHadronCollider lhc = initLHC();
+        assertNotNull(lhc.getRing());
+        assertNotNull(lhc.getUsp1());
+        assertNotNull(lhc.getUsp2());
+
+    }
+
+    @Order(2)
+    @Test
+    void test2(){
+        LargeHadronCollider lhc = initLHC();
+
+
+
+    }
+
+    private LargeHadronCollider initLHC(){
+        LargeHadronCollider lhc=new LargeHadronCollider();
+        Building building=new Building();
+        Ring ring=new Ring();
+        USP usp1=new USP();
+        USP usp2=new USP();
+        lhc.setBuilding(building);
+        lhc.setRing(ring);
+        lhc.setUsp1(usp1);
+        lhc.setUsp2(usp2);
+        return lhc;
+    }
+}
